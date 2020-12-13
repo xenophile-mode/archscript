7dbd432ac5b9325b09b5ff38d9f33a90fc237c3e
xenophile-mode <erickgpersonalacct1@gmail.com>
2020-12-13 10:32:52 -0600

    updated pkglist generation script

--- pl.sh
+++ pl.sh
@@ -4,3 +4,7 @@ pacman -Qqen > pkglist.txt
 pacman -Qqem > localpkglist.txt
 mv ~/pkglist.txt ~/archscript
 mv ~/localpkglist.txt ~/archscript
+cd archscript
+git add pkglist.txt localpkglist.txt
+git commit -m "updated pkglists"
+git push
