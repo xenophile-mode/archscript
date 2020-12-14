9547b737534b5e52a8552779a0bab38724ea4b7a
xenophile-mode <erickgpersonalacct1@gmail.com>
2020-12-14 07:22:04 -0600

    arch install script test

--- archscriptwinstall.sh
+++ archscriptwinstall.sh
@@ -73,19 +73,19 @@ swapon /dev/sda2
 # Install Arch Linux
 echo "Starting install.."
 echo "Installing Arch Linux, KDE with Konsole and Dolphin and GRUB2 as bootloader" 
-pacstrap /mnt base base-devel zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools freetype2 fuse2 mtools iw wpa_supplicant dialog xorg xorg-server xorg-xinit mesa xf86-video-intel plasma konsole dolphin
+pacstrap /mnt base base-devel zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools freetype2 fuse2 mtools iw wpa_supplicant dialog xorg xorg-server xorg-xinit mesa xf86-video-intel
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Copy post-install system cinfiguration script to new /root
-cp -rfv post-install.sh /mnt/root
-chmod a+x /mnt/root/post-install.sh
-cp ~/archscript/archscriptwinstall2.sh /mnt 
+cp -rfv ~/archscript/archscriptwinstall2.sh /mnt/root
+chmod a+x /mnt/root/archscriptwinstall2.sh
+#cp ~/archscript/archscriptwinstall2.sh /mnt 
 
 # Chroot into new system
 echo "After chrooting into newly installed OS, please run the post-install.sh by executing ./post-install.sh"
 echo "Press any key to chroot..."
 read tmpvar
-sudo arch-chroot /mnt sudo ./archscriptwinstall2.sh
+#sudo arch-chroot /mnt sudo ./archscriptwinstall2.sh
 
