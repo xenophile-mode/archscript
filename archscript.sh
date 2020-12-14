25c45c6806f917c812381a31ad8aa2099ea61edb
xenophile-mode <erickgpersonalacct1@gmail.com>
2020-12-14 15:06:04 -0600

    fish fix

--- archscript.sh
+++ archscript.sh
@@ -112,7 +112,7 @@ mkdir functions
 cd
 cp ~/archscript/dotfiles/fish/fish_import_bash_aliases.fish ~/.config/fish/functions
 cd ~/.config/fish/functions
-fish_import_bash_aliases
+fish_import_bash_aliases.fish
 
 #Configure termite
 cp ~/archscript/dotfiles/terms/termite/config ~/.config/termite
